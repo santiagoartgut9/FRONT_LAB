58cafa0c10f67088f561f659d07f4bb0ea8a893f
santiagoartgut9 <69946591+santiagoartgut9@users.noreply.github.com>
2025-04-03 02:16:43 -0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow">
       <div className="container">
-        <a className="navbar-brand d-flex align-items-center" href="#">
+        <a className="navbar-brand d-flex align-items-center" href="/">
           <img src="logo.jpg" alt="Logo" className="logo me-2" />
           &nbsp;&nbsp;&nbsp;Reserva de Laboratorios
         </a>
